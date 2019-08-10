0ef5b5217d1122b023ded8718425b26bb0b11e2a
Devan Potter-Bonar <devan.potter.bonar@gmail.com>
2019-08-10 19:18:47 +0000

    Troubleshooting FactoryBot user issue

--- spec/helpers/comments_helper_spec.rb
+++ spec/helpers/comments_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the GramsHelper. For example:
+# the CommentsHelper. For example:
 #
-# describe GramsHelper do
+# describe CommentsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe GramsHelper, type: :helper do
-  
+RSpec.describe CommentsHelper, type: :helper do
+ 
 end
